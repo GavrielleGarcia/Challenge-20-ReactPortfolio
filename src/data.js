a76ed2624f12c5d5ea512d84288363ee93ff5609
Gabriela Garcia <107376338+GavrielleGarcia@users.noreply.github.com>
2022-11-27 21:36:30 -0600

    upload local files

---
+++ src/data.js
@@ -0,0 +1,44 @@
+export const projects = [
+  {
+    title: "Password Generator",
+    subtitle: "Javascript",
+    description:
+      "A password generator with the option toselect lenght and symbols with a lenght of 10 to 18 characters",
+    image: "./PWGenerator.png",
+    //link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Book Searcher",
+    subtitle: "MERN",
+    description:
+      "A MERN application to search for book using API and React",
+    image: "./BookSearcher.gif",
+    //link: "https://reactbootcamp.com",
+  },
+  {
+    title: "The Mind a Developer Blog",
+    subtitle: "MySQL, Node & Express",
+    description:
+      "A Blog site to comment about the mind of a Developer.",
+    image: "./BlogSite.gif",
+    //link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Note Taker",
+    subtitle: "MySQL, Node & Express",
+    description:
+      "A note taker to submit task during the day that need to be completed between a time fram o 8 am to 6 pm",
+    image: "./NoteTaker.jpg",
+    //link: "https://reactbootcamp.com",
+  },
+];
+
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Express",
+  "Node",
+  "GraphQL",
+  "MySQL",
+];
